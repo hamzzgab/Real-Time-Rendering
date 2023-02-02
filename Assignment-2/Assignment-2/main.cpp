b3452aa15830e48856742a6d644ee4e959122b31
flyingtatta <hamzajg16@gmail.com>
2023-02-02 13:58:52 +0000

    Adding Assignment-2

---
+++ Assignment-2/Assignment-2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment-2
+//
+//  Created by Hamza Gabajiwala on 02/02/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
